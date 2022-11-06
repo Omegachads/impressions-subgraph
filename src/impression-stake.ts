1a172a316229dfca41c5b94846ab36d9b1603a0b
Kenk <kenghin_lim@hotmail.com>
2022-11-06 13:00:05 +0000

    Add PUSH

--- src/impression-stake.ts
+++ src/impression-stake.ts
@@ -6,6 +6,7 @@ import {
 
 import { BigInt } from "@graphprotocol/graph-ts";
 import { RequestMessage } from "../generated/schema";
+import { sendEPNSNotification } from "./EPNSNotification";
 
 export function handleMessageRequestCreated(
   event: MessageRequestCreated
@@ -15,13 +16,26 @@ export function handleMessageRequestCreated(
   let entity = new RequestMessage(event.transaction.from.toHex());
 
   // Entity fields can be set based on event parameters
-  entity.messageHash = event.params.requestId;
+  entity.messageHash = event.params.msgHash;
   entity.from = event.params.from;
   entity.to = event.params.to;
   entity.amount = event.params.amount;
 
   // Entities can be written to the store with `.save()`
   entity.save();
+
+  let recipient = "0xB0853B57326e08aA536663D6aC78304c0b3E38Da",
+    type = "1",
+    title = "New AD Received",
+    body = `${event.params.msgHash}`,
+    subject = "New AD Received",
+    message = `${event.params.msgHash}`,
+    image = "null",
+    secret = "null",
+    cta = "";
+
+  let notification = `{\"type\": \"${type}\", \"title\": \"${title}\", \"body\": \"${body}\", \"subject\": \"${subject}\", \"message\": \"${message}\", \"image\": \"${image}\", \"secret\": \"${secret}\", \"cta\": \"${cta}\"}`;
+  sendEPNSNotification(recipient, notification);
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
