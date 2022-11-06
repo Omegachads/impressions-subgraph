bff50d17c16ae5dfe4f9aab66514b4aebe6d7352
Kenk <kenghin_lim@hotmail.com>
2022-11-06 11:48:45 +0000

    update impression-stake.ts

--- src/impression-stake.ts
+++ src/impression-stake.ts
@@ -1,18 +1,21 @@
-import { BigInt } from '@graphprotocol/graph-ts';
 import {
   ImpressionStake,
   MessageRequestCreated,
-  OwnershipTransferred
-} from '../generated/ImpressionStake/ImpressionStake';
-import { RequestMessage } from '../generated/schema';
+  OwnershipTransferred,
+} from "../generated/ImpressionStake/ImpressionStake";
 
-export function handleMessageRequestCreated(event: MessageRequestCreated): void {
+import { BigInt } from "@graphprotocol/graph-ts";
+import { RequestMessage } from "../generated/schema";
+
+export function handleMessageRequestCreated(
+  event: MessageRequestCreated
+): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
   let entity = new RequestMessage(event.transaction.from.toHex());
 
   // Entity fields can be set based on event parameters
-  entity.requestId = event.params.requestId;
+  entity.messageHash = event.params.requestId;
   entity.from = event.params.from;
   entity.to = event.params.to;
   entity.amount = event.params.amount;
